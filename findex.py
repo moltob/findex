d4a0d5243fcaf9dbfbff4c4fe884861484fb8a13
Mike Pagel <mike@mpagel.de>
2020-04-19 16:16:03 +0200

    logging, timing

--- findex.py
+++ findex.py
@@ -1,11 +1,16 @@
 """Index of files in a directory structure."""
 import collections
+import hashlib
 import logging
+import mmap
+import os
 import pathlib
 import sqlite3
+import typing as t
 
-FILEHASH_EMPTY = '0000000000000000000000000000000000000000'
-"""Fake hash for empty files."""
+# fake hash values to identify non-hashable files:
+FILEHASH_EMPTY = 'empty'.ljust(40, '.')
+FILEHASH_INACCESSIBLE = 'inaccessible'.ljust(40, '.')
 
 FileDesc = collections.namedtuple('FileDesc', 'path size fhash')
 """Descriptor for a file in index."""
@@ -42,12 +47,63 @@ class Index:
 
     def open(self):
         if self.opened:
-            _logger.error('Database already open.')
+            _logger.warning('Database already open.')
             return
 
+        _logger.debug(f'Opening database {self.path}.')
         self.connection = sqlite3.connect(self.path)
 
     def close(self):
         if self.opened:
             self.connection.close()
             self.connection = None
+            _logger.debug('Database closed.')
+
+    def add_directory(self, path: pathlib.Path):
+        """Adds the files in directory to index."""
+        if not self.opened:
+            _logger.error('Index is not opened, cannot add data.')
+            return
+
+        _logger.info(f'Adding {path} to index.')
+
+
+def count_files(top: pathlib.Path) -> int:
+    count = 0
+    for dirpath, dirnames, filenames in os.walk(top):
+        count += len(filenames)
+    return count
+
+
+def count_bytes(top: pathlib.Path) -> int:
+    count = 0
+    for dirpath, dirnames, filenames in os.walk(top):
+        for filename in filenames:
+            count += (top / dirpath / filename).stat().st_size
+    return count
+
+
+def index_directory(top: pathlib.Path) -> t.Iterable[t.Tuple[str, pathlib.Path, int]]:
+    """Recurse given directory and for each non-empty file return content hash and path."""
+    _logger.info(f'Traversing directory {top} recursively.')
+
+    for dirpath, dirnames, filenames in os.walk(top):
+        root = pathlib.Path(dirpath).absolute()
+        for filename in filenames:
+            filepath = root / filename
+            filesize = filepath.stat().st_size
+
+            if filesize == 0:
+                filehash = FILEHASH_EMPTY
+            else:
+                filehash = compute_filehash(filepath)
+
+            _logger.debug(f'{filehash} {filepath}')
+            yield FileDesc(path=filepath, fhash=filehash, size=filesize)
+
+
+def compute_filehash(filepath: pathlib.Path) -> str:
+    with open(filepath, 'rb') as file:
+        with mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as data:
+            sha1 = hashlib.sha1(data)
+    return sha1.hexdigest()
--- main.py
+++ main.py
@@ -1,35 +1,33 @@
+import logging
 import pathlib
 
-from fhash import index_directory, count_files
+from findex import index_directory, count_files, count_bytes
 
 
 def main():
+    import colorama
+    colorama.init()
+
+    import daiquiri
+    daiquiri.setup(level=logging.DEBUG)
+
     # findex = Index(pathlib.Path('test.db'))
     # findex.create()
 
     TEST_DIR = pathlib.Path(r'Q:\Britta Pagel Fotografie')
 
-    result = None
-
     def timed_main():
-        nonlocal result
-        result = list(index_directory(TEST_DIR))
+        list(index_directory(TEST_DIR))
 
     import timeit
 
-    count_time = timeit.timeit(lambda: count_files(TEST_DIR), number=1)
+    files_time = timeit.timeit(lambda: count_files(TEST_DIR), number=1)
+    bytes_time = timeit.timeit(lambda: count_bytes(TEST_DIR), number=1)
     exec_time = timeit.timeit(timed_main, number=1)
 
-    numbytes = 0
-    for fdesc in result:
-        print(fdesc.fhash, fdesc.path)
-        numbytes += fdesc.size
-
-    kbits = 8 / 1024 * numbytes / exec_time
-    print(f'Count time:     {count_time:.3f} s')
+    print(f'Count files:    {files_time:.3f} s')
+    print(f'Count bytes:    {bytes_time:.3f} s')
     print(f'Execution time: {exec_time:.3f} s')
-    print(f'Files:          {len(result)} ({numbytes:,d} bytes)')
-    print(f'Bandwith:       {kbits:.2f} kbit/s')
 
 
 if __name__ == '__main__':

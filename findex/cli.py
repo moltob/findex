f97ce76bcbafee468bd5fdbb49402fde14697722
Mike Pagel <mike@mpagel.de>
2020-04-20 19:00:09 +0200

    add CLI arguments

--- findex/cli.py
+++ findex/cli.py
@@ -1,26 +1,45 @@
+import argparse
 import logging
 import os
 import pathlib
 
 from findex.files import Index
 
-INDEX_DIR = pathlib.Path(r'.')
-INDEX_DB = pathlib.Path('test.db')
-
 
 def main():
+    parser = argparse.ArgumentParser(description='Hash-based directory indexer')
+    parser.add_argument(
+        'directory',
+        help='Path to directory for which index is created.',
+    )
+    parser.add_argument(
+        '--index',
+        help='Path to created index file.',
+        default='findex.db'
+    )
+    parser.add_argument(
+        '--overwrite',
+        help='Flag whether to overwrite an existing index file. Previous data will be lost.',
+        action='store_true',
+    )
+
     import colorama
     colorama.init()
 
     import daiquiri
     daiquiri.setup(level=logging.INFO)
 
-    if 'INDEX_DEVELOPER' in os.environ:
-        INDEX_DB.unlink(missing_ok=True)
+    args = parser.parse_args()
 
-    findex = Index(pathlib.Path(INDEX_DB))
+    index_path = pathlib.Path(args.index)
+    directory_path = pathlib.Path(args.directory)
+
+    if args.overwrite:
+        index_path.unlink(missing_ok=True)
+
+    findex = Index(index_path)
     findex.create()
-    findex.add_directory(INDEX_DIR)
+    findex.add_directory(directory_path)
 
 
 if __name__ == '__main__':

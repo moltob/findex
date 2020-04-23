d2b5f01458acfac31d226fc4b62a3518ba951ca3
Mike Pagel <mike@mpagel.de>
2020-04-23 19:26:44 +0200

    add os errors to database

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='findex',
-    version='0.1.0',
+    version='0.2.0',
     description='Hash-based indices of directories.',
     author='Mike Pagel',
     author_email='mike@mpagel.de',

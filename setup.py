060eb42d27f6c54b436bb511bd46cffd71bc7bed
Mike Pagel <mike@mpagel.de>
2020-05-10 11:36:42 +0200

    upped version to 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="findex",
-    version="1.0.0",
+    version="1.0.4",
     description="Hash-based indices of directories.",
     author="Mike Pagel",
     author_email="mike@mpagel.de",

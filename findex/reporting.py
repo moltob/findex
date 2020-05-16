4b7e8595b5d889406fc949c395b782062f5ad2bc
Mike Pagel <mike@mpagel.de>
2020-05-16 10:58:49 +0200

    report missing, updated and new files

--- findex/reporting.py
+++ findex/reporting.py
@@ -34,21 +34,27 @@ class ComparisonReport:
                 ),
             }
 
-            self._write_new()
+            self._write_files_worksheet("Missing Files", self.comparison.iter_missing())
+            self._write_files_worksheet("Updated Files", self.comparison.iter_updated())
+            self._write_files_worksheet("New Files", self.comparison.iter_new())
 
         self.workbook = self.formats = None
 
-    def _write_new(self):
-        worksheet = self.workbook.add_worksheet("New Files")
+    def _write_files_worksheet(self, worksheet_name, files):
+        data = [(file.path, file.size, file.created, file.modified, file.fhash) for file in files]
 
-        worksheet.set_column(0, 0, width=80)
+        if not data:
+            _logger.warning(f'Skipping worksheet {worksheet_name!r} as data set is empty.')
+            return
+
+        _logger.debug(f'Creating worksheet {worksheet_name!r} with {len(data)} entries.')
+
+        worksheet = self.workbook.add_worksheet(worksheet_name)
+
+        worksheet.set_column(0, 0, width=120)
         worksheet.set_column(1, 1, width=15)
         worksheet.set_column(2, 3, width=25)
         worksheet.set_column(4, 4, width=50)
-
-        data = []
-        for file in self.comparison.iter_updated():
-            data.append((file.path, file.size, file.created, file.modified, file.fhash))
 
         worksheet.add_table(
             0,
